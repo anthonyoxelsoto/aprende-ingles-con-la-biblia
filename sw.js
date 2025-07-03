e7aadf2af5685d3c0a2431e899fba86af5a8da26
anthonyoxelsoto <anthonyoxelsoto@gmail.com>
2025-07-02 22:51:04 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'audio-biblia-v1';
+const urlsToCache = [
+  './',
+  './audio-bible-pwa-complete.html',
+  './manifest.json',
+  './favicon.ico'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
